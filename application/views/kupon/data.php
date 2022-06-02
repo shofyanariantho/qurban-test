1b7aa9cc6e37103f248c6f10a72e7ef2ee35e971
shofyanariantho <91594761+shofyanariantho@users.noreply.github.com>
2022-06-02 11:24:48 +0700

    Update Data

--- application/views/kupon/data.php
+++ application/views/kupon/data.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col">
                 <h4 class="h5 align-middle m-0 font-weight-bold text-primary">
-                    Data Kupon Qurban
+                    Data Kupon Qurban Sapi
                 </h4>
             </div>
             <div class="col-auto">
